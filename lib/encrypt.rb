b28a341a86deb6ffc26d4fbe5cd902392229217a
Angel Breaux <abreaux@detaso.com>
2021-01-15 10:55:08 -0600

    Add offset

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,27 +1,41 @@
 class Encrypt
   attr_reader :phrase,
               :key,
-              :date
+              :date,
+              :character_set
 
-  def initialize(phrase, key, date = Time.now)
+  def initialize(phrase, key, date = Date.today.strftime('%d%m%y'))
     @phrase = phrase
     @key = key
     @date = date
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def offset
+    (@date.to_i ** 2).to_s[-4..-1]
   end
 
   def a_shift
-    @key[0..1]
+    @key[0..1].to_i + offset[0].to_i
   end
 
   def b_shift
-    @key[1..2]
+    @key[1..2].to_i + offset[1].to_i
   end
 
   def c_shift
-    @key[2..3]
+    @key[2..3].to_i + offset[2].to_i
   end
 
   def d_shift
-    @key[3..4]
+    @key[3..4].to_i + offset[3].to_i
+  end
+
+  def encrypt
+    {
+      encryption: "keder ohulw",
+      key: @key,
+      date: @date
+    }
   end
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -14,21 +14,36 @@ class EncryptTest < Minitest::Test
     assert_equal "hello world", @encrypt.phrase
     assert_equal "02715", @encrypt.key
     assert_equal "040895", @encrypt.date
+    assert_equal ("a".."z").to_a << " ", @encrypt.character_set
+  end
+
+  def test_offset
+    assert_equal "1025", @encrypt.offset
   end
 
   def test_a_shift
-    assert_equal "02", @encrypt.a_shift
+    assert_equal 3, @encrypt.a_shift
   end
 
   def test_b_shift
-    assert_equal "27", @encrypt.b_shift
+    assert_equal 27, @encrypt.b_shift
   end
 
   def test_c_shift
-    assert_equal "71", @encrypt.c_shift
+    assert_equal 73, @encrypt.c_shift
   end
 
   def test_d_shift
-    assert_equal "15", @encrypt.d_shift
+    assert_equal 20, @encrypt.d_shift
+  end
+
+  def test_encrypt
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @encrypt.encrypt
   end
 end
