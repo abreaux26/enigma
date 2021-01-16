2ab1ab08cb0a428fefe2c76ec6b5c67414cdca1e
Angel Breaux <48256547+abreaux26@users.noreply.github.com>
2021-01-16 14:00:42 -0600

    Add decrypt runner file (#9)
    Co-authored-by: Angel Breaux <abreaux@detaso.com>

---
+++ lib/decrypt.rb
@@ -0,0 +1,10 @@
+require_relative 'enigma'
+
+file = File.open(ARGV[0])
+message_to_decrypt = file.read.chomp
+key = ARGV[2]
+date = ARGV[3]
+decrypt_hash = Enigma.decrypt(message_to_decrypt, key, date)
+write_to_file = ARGV[1]
+decrypted =  File.write(write_to_file, decrypt_hash[:decryption])
+puts "Created '#{write_to_file}' with the key #{key} and date #{date}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,4 +5,4 @@ message_to_encrypt = file.read.chomp
 encrypt_hash = Enigma.encrypt(message_to_encrypt, "02715", "040895")
 write_to_file = ARGV[1]
 encrypted =  File.write(write_to_file, encrypt_hash[:encryption])
-puts "Created #{write_to_file} with the key #{encrypt_hash[:key]} and date #{encrypt_hash[:date]}"
+puts "Created '#{write_to_file}' with the key #{encrypt_hash[:key]} and date #{encrypt_hash[:date]}"
