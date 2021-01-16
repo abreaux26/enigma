b69338998866e491e01c68bf00a513830b05fd6a
Angel Breaux <abreaux@detaso.com>
2021-01-16 11:11:47 -0600

    Memoize key

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -26,9 +26,8 @@ class Encrypt
   def key
     if @key == 0
       @key = random_number
-    else
-      @key
     end
+    @_key ||= @key
   end
 
   def offset
@@ -36,19 +35,19 @@ class Encrypt
   end
 
   def a_shift
-    @key[0..1].to_i + offset[0].to_i
+    key[0..1].to_i + offset[0].to_i
   end
 
   def b_shift
-    @key[1..2].to_i + offset[1].to_i
+    key[1..2].to_i + offset[1].to_i
   end
 
   def c_shift
-    @key[2..3].to_i + offset[2].to_i
+    key[2..3].to_i + offset[2].to_i
   end
 
   def d_shift
-    @key[3..4].to_i + offset[3].to_i
+    key[3..4].to_i + offset[3].to_i
   end
 
   def phrase_encryption
