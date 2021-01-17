fe70330141ae30743d2290ef909cd4a22f986a81
Angel Breaux <abreaux@detaso.com>
2021-01-16 18:18:55 -0600

    Refactor enigma to use new translate message class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,15 +1,16 @@
-require_relative 'encrypt_message'
-require_relative 'decrypt_message'
+# require_relative 'encrypt_message'
+# require_relative 'decrypt_message'
+require_relative 'translate_message'
 require_relative 'randomable'
 
 class Enigma
   include Randomable
 
   def self.encrypt(message, key = random_number, date = Date.today.strftime('%d%m%y'))
-    EncryptMessage.new(message, key, date).encrypt
+    TranslateMessage.new(message, key, date).encrypt
   end
 
-  def self.decrypt(message, key, date)
-    DecryptMessage.new(message, key, date).decrypt
+  def self.decrypt(ciphertext, key, date)
+    TranslateMessage.new(ciphertext, key, date).decrypt
   end
 end
