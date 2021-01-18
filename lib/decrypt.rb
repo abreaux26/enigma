5d6119020b753ee8dda8ed22b8d7f981cf15f73f
Angel Breaux <abreaux@detaso.com>
2021-01-18 11:45:13 -0600

    Fix variable names

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,22 +1,22 @@
 require_relative 'enigma'
 
-file = File.open(ARGV[0])
-write_to_file = ARGV[1]
+read_file = File.open(ARGV[0])
+write_file = ARGV[1]
 key = ARGV[2]
-date = ARGV[3]
-
-enigma = Enigma.new
-message_to_decrypt = file.read.chomp
 
 if key.nil?
   return puts 'Error! Need key to decrypt!'
 end
 
+date = ARGV[3]
+enigma = Enigma.new
+ciphertext = read_file.read.chomp
+
 if date.nil?
-  decrypt_hash = enigma.decrypt(message_to_decrypt, key)
+  decrypted = enigma.decrypt(ciphertext, key)
 else
-  decrypt_hash = enigma.decrypt(message_to_decrypt, key, date)
+  decrypted = enigma.decrypt(ciphertext, key, date)
 end
 
-decrypted =  File.write(write_to_file, decrypt_hash[:decryption])
-puts "Created '#{write_to_file}' with the key #{decrypt_hash[:key]} and date #{decrypt_hash[:date]}"
+File.write(write_file, decrypted[:decryption])
+puts "Created '#{write_file}' with the key #{decrypted[:key]} and date #{decrypted[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,12 @@
 require_relative 'enigma'
 
-file = File.open(ARGV[0])
-write_to_file = ARGV[1]
+read_file = File.open(ARGV[0])
+write_file = ARGV[1]
 
 enigma = Enigma.new
-message_to_encrypt = file.read.chomp
+message_to_encrypt = read_file.read.chomp
 
-encrypt_hash = enigma.encrypt(message_to_encrypt)
+encrypted = enigma.encrypt(message_to_encrypt)
 
-encrypted =  File.write(write_to_file, encrypt_hash[:encryption])
-puts "Created '#{write_to_file}' with the key #{encrypt_hash[:key]} and date #{encrypt_hash[:date]}"
+File.write(write_file, encrypted[:encryption])
+puts "Created '#{write_file}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
