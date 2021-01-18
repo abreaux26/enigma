279b59c1bbb0cd4b898d844cd753f66754639adb
Angel Breaux <abreaux@detaso.com>
2021-01-17 21:12:47 -0600

    Fix nil date passing in

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,7 +5,14 @@ message_to_decrypt = file.read.chomp
 key = ARGV[2]
 date = ARGV[3]
 enigma = Enigma.new
-decrypt_hash = enigma.decrypt(message_to_decrypt, key, date)
+if key.nil?
+  return puts "Error! Need key to decrypt!"
+end
+if date.nil?
+  decrypt_hash = enigma.decrypt(message_to_decrypt, key)
+else
+  decrypt_hash = enigma.decrypt(message_to_decrypt, key, date)
+end
 write_to_file = ARGV[1]
 decrypted =  File.write(write_to_file, decrypt_hash[:decryption])
-puts "Created '#{write_to_file}' with the key #{key} and date #{date}"
+puts "Created '#{write_to_file}' with the key #{decrypt_hash[:key]} and date #{decrypt_hash[:date]}"
